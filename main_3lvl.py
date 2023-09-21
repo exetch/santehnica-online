b1953a96bcac2e9a872a9ff66e8179ca8d60ef51
exetch <gassemark@gmail.com>
2023-09-21 13:02:42 +0300

    3lvl comments added

--- main_3lvl.py
+++ main_3lvl.py
@@ -7,23 +7,52 @@ import time
 
 # Функция для проверки наличия размеров или "_sm" в ссылке и значении
 def has_size_or_sm_pattern(link, value):
+    """
+    Проверяет, содержит ли ссылка или значение шаблон размера или "sm".
+
+    Args:
+        link (str): Строка, представляющая ссылку.
+        value (str): Строка, представляющая значение.
+
+    Returns:
+        bool: True, если ссылка или значение содержат шаблон размера или "sm", иначе False.
+    """
+    # Шаблон для размера в формате "123x456"
     size_pattern = r'\d{2,3}x\d{2,3}'
+
+    # Шаблон для "sm" в формате "123_sm"
     sm_pattern = r"\d{2,3}_sm"
+
+    # Проверяем наличие шаблона размера и "sm" в ссылке и значении
     is_link_has_size = re.search(size_pattern, link)
     is_link_has_sm = re.search(sm_pattern, link)
     is_value_has_size = re.search(size_pattern, value)
     is_value_has_sm = re.search(sm_pattern, value)
-    return (is_link_has_size and (is_value_has_sm or is_value_has_size)) or (is_link_has_sm and (is_value_has_sm or is_value_has_size))
+
+    # Возвращаем True, если хотя бы одно из условий выполняется
+    return (is_link_has_size and (is_value_has_sm or is_value_has_size)) or (
+                is_link_has_sm and (is_value_has_sm or is_value_has_size))
 def process_link(link, working_links):
+    """
+    Обрабатывает одну ссылку, проверяя наличие страницы "404 Not Found" и извлекая рабочие ссылки.
+
+    Args:
+        link (str): Ссылка для проверки.
+        working_links (dict): Словарь, в котором сохраняются рабочие ссылки.
+
+    Returns:
+        str or None: Возвращает рабочую ссылку или None, если ссылка не существует.
+    """
     try:
+        # Создаем экземпляр браузера
         driver = webdriver.Chrome()
         driver.get(link)
         time.sleep(2)
 
-        # Проверяем <title> на странице
+        # Проверяем <title> на странице на наличие "404 Not Found"
         if driver.title == "404 Not Found":
             print(f"Ссылка не существует: {link}")
-            return
+            return None
 
         # Получаем HTML-код страницы
         page_source = driver.page_source
@@ -40,19 +69,39 @@ def process_link(link, working_links):
     except Exception as e:
         pass
     finally:
+        # Закрываем браузер после завершения обработки
         driver.quit()
 
+
 def data_processing_3lvl_parallel(working_links_1lvl, working_links_1lv2, file_output, num_threads=5):
+    """
+    Обрабатывает ссылки третьего уровня, используя рабочие ссылки первого и второго уровней,
+    и сохраняет рабочие ссылки третьего уровня в файл.
+
+    Args:
+        working_links_1lvl (str): Путь к файлу с рабочими ссылками первого уровня (JSON формат).
+        working_links_1lv2 (str): Путь к файлу с рабочими ссылками второго уровня (JSON формат).
+        file_output (str): Путь к файлу, в который будут сохранены рабочие ссылки третьего уровня (JSON формат).
+        num_threads (int, optional): Количество потоков для параллельной обработки ссылок. По умолчанию 5.
+
+    Returns:
+        None
+    """
+    # Загрузка рабочих ссылок первого уровня
     with open(working_links_1lvl, 'r', encoding='utf-8') as file:
         data_dict_1lvl = json.load(file)
         refs_data = {}
         for key, url in data_dict_1lvl.items():
             last_part = url.split('/')[-1]
             refs_data[key] = last_part
+
+    # Загрузка рабочих ссылок второго уровня
     with open(working_links_1lv2, 'r', encoding='utf-8') as refs_file:
         data_dict_2lvl = json.load(refs_file)
+
     filtered_links = {}
 
+    # Фильтрация ссылок третьего уровня
     for link_name, link_url in data_dict_2lvl.items():
         for name, value in refs_data.items():
             if not 'brand' in link_url and (value not in link_url) and not has_size_or_sm_pattern(link_url, value):
@@ -77,6 +126,7 @@ def data_processing_3lvl_parallel(working_links_1lvl, working_links_1lv2, file_o
                 print(f"Прогресс: {progress_percent:.2f}% ({processed_links}/{len(links_to_check)})")
                 print(f"Всего рабочих ссылок {good_links} ")
 
+    # Сохранение рабочих ссылок третьего уровня в файл
     with open(file_output, 'w', encoding='utf-8') as json_file:
         json.dump(working_links, json_file, ensure_ascii=False, indent=4)
     print(f"Третий уровень ссылок успешно сохранен в {file_output}")
